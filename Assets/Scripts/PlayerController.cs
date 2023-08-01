f9e7142b693da5a996ffe1a9ccf90d7d4614e4af
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2023-08-01 12:42:21 +0300

    tokan päivän puoliväli

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D body;
+    // input-muuttujat
+    private float horizontalMovement;
+    // liikkumismuuttujat
+    private float moveSpeed = 5f;
+    private Vector2 movement = new Vector2();
+    // hyppäämismuuttujat
+    private float jumpForce = 10f;
+    private bool grounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        movement.x = horizontalMovement * moveSpeed;
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(movement * Time.deltaTime);
+    }
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = false;
+        }
+    }
+}
